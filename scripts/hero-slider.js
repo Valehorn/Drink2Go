d2dfc5c0c19690b17f54caecd960c1378d590abf
Valehorn <pakri@yandex.ru>
2024-06-18 09:33:25 +0000

    Deploying to gh-pages from @ Valehorn/Drink2Go@119b5473981a0af5c4631c65e71fd7a43b6f0abf 🚀

---
+++ scripts/hero-slider.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".slider"),t=document.querySelectorAll(".slider__slide"),o=document.querySelector(".slider-button-next"),n=document.querySelector(".slider-button-next");

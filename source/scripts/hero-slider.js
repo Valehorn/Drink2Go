119b5473981a0af5c4631c65e71fd7a43b6f0abf
Пакри <pakri@yandex.ru>
2024-06-18 12:32:53 +0300

    промежуточный коммит

---
+++ source/scripts/hero-slider.js
@@ -0,0 +1,4 @@
+const slider = document.querySelector('.slider');
+const slide = document.querySelectorAll('.slider__slide');
+const buttonNext = document.querySelector('.slider-button-next');
+const buttonPrev = document.querySelector('.slider-button-next');

f005b097ad2e5d6e2fe8f81b4fb9052440bb6903
Пакри <pakri@yandex.ru>
2024-06-20 15:57:07 +0300

    Реализовал передачу значений для noUIslider цены.

--- source/scripts/price.js
+++ source/scripts/price.js
@@ -1,11 +1,35 @@
 import { sliderElement } from './slider';
 
+const priceInputFrom = document.querySelector('#price-from');
+const priceInputTo = document.querySelector('#price-to');
+
 const onSliderUpdate = () => {
-  const priceInputFrom = document.querySelector('#price-from');
-  const priceInputTo = document.querySelector('#price-to');
   const [valueLower, valueUpper] = sliderElement.noUiSlider.get(true);
   priceInputFrom.value = valueLower;
   priceInputTo.value = valueUpper;
 };
 
-export {onSliderUpdate};
+
+const onSliderSlide = (values, handle) => {
+  const value = values[handle];
+  if (handle === 0) {
+    priceInputFrom.value = value;
+  } else {
+    priceInputTo.value = value;
+  }
+};
+
+sliderElement.noUiSlider.on('slide', onSliderSlide);
+
+const onInputFrom = () => {
+  sliderElement.noUiSlider.set([priceInputFrom.value, null]);
+};
+
+const onInputTo = () => {
+  sliderElement.noUiSlider.set([null, priceInputTo.value]);
+};
+
+priceInputFrom.addEventListener('change', onInputFrom);
+priceInputTo.addEventListener('change', onInputTo);
+
+export { onSliderUpdate };
